fb35274d51e3afc217e802052b0362d840bca095
Ben Endara <bendara88@gmail.com>
2020-11-30 13:59:48 -0600

    Added Chip struct and functions

--- simulator/hdr/chip.h
+++ simulator/hdr/chip.h
@@ -1,12 +1,15 @@
 #ifndef HDR_CHIP
 #define HDR_CHIP
+#include <stddef.h>
 
 enum ChipType {
+	CHIP_74HC00,
 	CHIP_74HC377,
 };
 
 enum ChipState {
-	STATE_
+	STATE_STABLE,
+	STATE_PROP_DELAY,
 };
 
 struct Net;
@@ -15,11 +18,41 @@ struct Chip {
 	enum ChipType type;
 	enum ChipState state;
 	long delay_ctr;
-	struct Net **in_arr, **out_arr;
+	struct {
+		struct Net **buf;
+		size_t len;
+	} in_arr, out_arr;
 	union {
-		char *mem_arr;
+		struct {
+			unsigned char *buf;
+			size_t len;
+		} mem_arr;
 		long val;
 	};
 };
+
+void initChip(
+	struct Chip *chip,
+	enum ChipType type,
+	size_t in_len,
+	size_t out_len
+);
+
+void initChipMemory(
+	struct Chip *chip,
+	size_t len
+);
+
+void stepChip(
+	struct Chip *chip
+);
+
+void step74HC00(
+	struct Chip *chip
+);
+
+void step74HC377(
+	struct Chip *chip
+);
 
 #endif

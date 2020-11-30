35f85721ad3e373f56147623f7ab7c1a7b9ab235
Ben Endara <bendara88@gmail.com>
2020-11-30 16:57:19 -0600

    Initial commit

---
+++ simulator/hdr/chip-data.h
@@ -0,0 +1,44 @@
+#ifndef HDR_CHIP_DATA
+#define HDR_CHIP_DATA
+#include <stddef.h>
+
+enum ChipType {
+	CHIP_74HC00, /*
+	 * Quad Nand Gate
+	 * 8 in
+	 * 4 out
+	 * in0,1: Gate A inputs
+	 * in2,3: Gate B inputs
+	 * in4,5: Gate C inputs
+	 * in6,7: Gate D inputs
+	 * out0: Gate A output
+	 * out1: Gate B output
+	 * out2: Gate C output
+	 * out3: Gate D output
+	 */
+	CHIP_74HC377, /*
+	 * 8-bit Register
+	 * 3 in
+	 * 1 out
+	 * in0: Data in
+	 * in1: ~Enable
+	 * in2: Clock
+	 * out0: Data out
+	 */
+};
+
+enum ChipState {
+	STATE_STABLE,
+	STATE_PROP_DELAY,
+};
+
+struct ChipData {
+	size_t in_len, out_len, mem_len;
+};
+
+const struct ChipData CHIP_DATA_TABLE[] = {
+	{8, 4, 0},
+	{3, 1, 0},
+};
+
+#endif

9f692588256c5f9423719ea038fc25749c84c509
Ben Endara <bendara88@gmail.com>
2020-12-13 00:56:45 -0600

    added file back

---
+++ simulator/hdr/token.h
@@ -0,0 +1,42 @@
+#ifndef HDR_TOKEN
+#define HDR_TOKEN
+#include <stddef.h>
+
+enum TokenType {
+	TOK_BUS,
+	TOK_NET,
+	TOK_CHIP,
+	TOK_SYMBOL,
+	TOK_NUM,
+	TOK_BEG_BRACE,
+	TOK_END_BRACE,
+	TOK_EQUALS,
+	TOK_COLON,
+	TOK_COMMA,
+	TOK_DASH,
+};
+
+struct Token {
+	enum TokenType type;
+	unsigned line;
+	union {
+		char *str;
+		unsigned num;
+	};
+};
+
+struct TokenVec {
+	struct Token *buf;
+	size_t cap, len;
+};
+
+void initTokenVec(
+	struct TokenVec *vec,
+	size_t cap
+);
+
+struct Token *newToken(
+	struct TokenVec *vec
+);
+
+#endif

15e1c621e8ed11cd28920ab4e5abd650ab9503b3
Ben Endara <bendara88@gmail.com>
2021-01-30 00:34:52 -0600

    added arguments module

---
+++ emulator/hdr/arguments.h
@@ -0,0 +1,20 @@
+#ifndef HDR_ARGUMENTS
+#define HDR_ARGUMENTS
+
+bool
+readArguments(
+	char **arg,
+	size_t arg_len
+);
+
+char *
+getFlashPath(
+	void
+);
+
+char *
+getMicrocodePath(
+	void
+);
+
+#endif

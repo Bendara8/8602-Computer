08f27811fdc620dd57cf1c1b8e9e7fc1e483a1fd
Ben Endara <bendara88@gmail.com>
2021-01-30 00:23:47 -0600

    added memory loader

--- emulator/src/memory.c
+++ emulator/src/memory.c
@@ -2,37 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static const size_t VRAM_SIZE = 32768, RAM_SIZE = 16384;
+static const size_t FLASH_SIZE = 262144, MICROCODE_SIZE = 3 * 524288;
+
 static uint8_t *vram, *ram, *bank, *flash, *microcode;
 
-static bool loadMemory(uint8_t *buffer, char *path);
+static bool loadMemory(uint8_t *buffer, size_t size, char *path);
 
 bool initMemory(char *flash_path, char *microcode_path) {
-	vram = malloc(32768);
+	vram = malloc(VRAM_SIZE);
 	if (!vram) {
 		puts("Could not allocate VRAM");
 		return false;
 	}
 
-	ram = malloc(16384);
+	ram = malloc(RAM_SIZE);
 	if (!ram) {
 		puts("Could not allocate RAM");
 		return false;
 	}
 
-	flash = malloc(262144);
+	flash = malloc(FLASH_SIZE);
 	if (!flash) {
 		puts("Could not allocate flash memory");
 		return false;
 	}
-	if (!loadMemory(flash, flash_path)) return false;
+	if (!loadMemory(flash, FLASH_SIZE, flash_path)) return false;
 	bank = flash;
 	
-	microcode = malloc(3 * 524288);
+	microcode = malloc(MICROCODE_SIZE);
 	if (!microcode) {
 		puts("Could not allocate microcode buffer");
 		return false;
 	}
-	if (!loadMemory(microcode, microcode_path)) return false;
+	if (!loadMemory(microcode, MICROCODE_SIZE, microcode_path)) return false;
 
 	return true;
 }
@@ -44,7 +47,21 @@ void deinitMemory(void) {
 	free(microcode);
 }
 
-bool loadMemory(uint8_t *buffer, char *path) {
+bool loadMemory(uint8_t *buffer, size_t size, char *path) {
+	FILE *file = fopen(path, "rb");
+	if (!file) {
+		printf("Unable to open '%s'\n", path);
+		return false;
+	}
+	for (size_t i = 0; i < size; ++i) {
+		int byte = getc(file);
+		if (byte == EOF) {
+			printf("File '%s' is too small, expected %zu bytes\n", path, size);
+			return false;
+		}
+		buffer[i] = (uint8_t)byte;
+	}
+	fclose(file);
 	return true;
 }
 

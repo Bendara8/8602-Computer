2b9df2320063e19b943deef4608d9f3657bd3c56
Ben Endara <bendara88@gmail.com>
2021-01-30 00:24:44 -0600

    added deinit calls

--- emulator/src/emulator.c
+++ emulator/src/emulator.c
@@ -1,13 +1,16 @@
 #include "display.h"
 #include "memory.h"
+#include <stdlib.h>
 
 int32_t main(void) {
 	if (!initMemory("res/flash.bin", "res/microcode.bin")) {
 		return 1;
 	}
+	atexit(deinitMemory);
 	if (!initDisplay(512, 256, 1, 16666667, getVRAM())) {
 		return 1;
 	}
-	for (uint64_t i = 0; i < 10000000000; ++i) {}
+	atexit(deinitDisplay);
+	for (uint64_t i = 0; i < 1000000000; ++i) {}
 	return 0;
 }
